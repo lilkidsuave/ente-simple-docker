b9b0ec8e0e8b05d8f317ced733b6ade33faa0278
lilkidsuave <66171643+lilkidsuave@users.noreply.github.com>
2024-07-09 12:17:21 -0400

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Exit on any error
+set -e
+
+# Clone the repository
+if [ ! -d "/data/ente/.git" ]; then
+    git clone https://github.com/ente-io/ente.git /data/ente
+    cd /data/ente/web
+    git submodule update --init --recursive
+else
+    cd /data/ente
+    git pull
+    cd /data/ente/web
+    git submodule update --recursive
+fi
+
+# Install dependencies
+yarn install
+
+# Build the application
+if [ -d "/data/ente/web/apps/photos/out" ]; then
+    echo "Removing previous build directory."
+    rm -rf /data/ente/web/apps/photos/out
+fi
+
+yarn build:photos
+
+# Execute the command provided as arguments to the entrypoint (if any)
+exec "$@"
